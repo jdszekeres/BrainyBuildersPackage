ef81287e90501a872d725ac048ab831c84fcadc5
jdszekeres <amyc21@yahoo.com>
2021-09-25 11:17:46 -0700

    lol

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import *
 app = Flask(__name__)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('/static', "favicon.ico")
 @app.route('/')
 def index():
     return render_template("index.html")
