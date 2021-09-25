bf53f5a29212ed08aec8f10df04cbbcdf145cae0
jdszekeres <amyc21@yahoo.com>
2021-09-25 11:30:24 -0700

    regular commit

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import *
+import os
 app = Flask(__name__)
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory('/static', "favicon.ico")
+@app.route("/favicon.ico")
+@app.route("/apple-touch-icon-precomposed.png")
+@app.route("/apple-touch-icon.png")
+@app.route("/favicon.png")
+@app.route("/favicon-coast.png")
+@app.route("/apple-touch-icon-<size>-precomposed.png")
+
+def favicon(size=""):
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 @app.route('/')
 def index():
     return render_template("index.html")
-app.run(debug=False, port=8000)
+app.run(debug=True, port=8000)
