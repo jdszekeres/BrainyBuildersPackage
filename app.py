0d433c1c12b4d9d7cc34dd2a7e26ede73d19f087
jdszekeres <amyc21@yahoo.com>
2021-09-29 16:38:28 -0700

    regular commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import *
 import os
+import db
+
 app = Flask(__name__)
 @app.route("/favicon.ico")
 @app.route("/apple-touch-icon-precomposed.png")
@@ -7,7 +9,6 @@ app = Flask(__name__)
 @app.route("/favicon.png")
 @app.route("/favicon-coast.png")
 @app.route("/apple-touch-icon-<size>-precomposed.png")
-
 def favicon(size=""):
     return send_from_directory(
         os.path.join(app.root_path, "static"),
@@ -16,6 +17,28 @@ def favicon(size=""):
     )
 @app.route('/')
 def index():
+    
     return render_template("index.html")
+@app.route("/package/<package>")
+def package(package):
+    if request.args.get("override") != None:
+            return render_template("not found.html")
+    info = db.select_package(package)
+    if info == None:
+        return render_template("not found.html")
+    print(info)
+    return render_template("package.html", info=info, locations=info[2])
+@app.route("/create/package", methods=["POST"])
+def create_package():
+    if request.method == "POST":
+        v = db.add_package([], request.form.get('adress'), id=request.form.get('id'))
+        print(request.form.get('id'))
+        return str(v)
+    else:
+        return "WIP"
+@app.route("/update/<package>", methods=["POST"])
+def update_package(package):
+    db.update_packages(package, request.form.get('newlocation'))
+    return redirect(request.referrer)
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8000)
---
+++ db.py
@@ -0,0 +1,37 @@
+url = "postgres://lwzpkrigadvrfh:8a8f7785ea98c6a63b1d0c82606ebdbbbc61ee72049c76a16d55537cdce3100d@ec2-34-233-105-94.compute-1.amazonaws.com:5432/d35v18mrti2v55"
+import psycopg2
+from datetime import datetime
+xconn = psycopg2.connect(url)
+cur = xconn.cursor()
+cur.execute("create table if not exists packages (id SERIAL, date text NOT NULL, locations jsonb NOT NULL, finaldestination text not null);")
+xconn.commit()
+del xconn
+def add_package(location, finallocation, id=None):
+    conn = psycopg2.connect(url)
+    cur = conn.cursor()
+    if id is None and select_package(__selectall()[-1][0]) is not None:
+        cur.execute("INSERT INTO packages (date, locations, finaldestination) VALUES ('{date}','{location}','{finaldestination}') RETURNING id;".format(date=datetime.now().strftime("%A %B %d %Y %r"), location=location, finaldestination=finallocation))
+    elif id != None:
+        cur.execute("INSERT INTO packages VALUES ('{id}','{date}','{location}','{finaldestination}') RETURNING id;".format(date=datetime.now().strftime("%A %B %d %Y %r"), location=location, finaldestination=finallocation, id=id))
+    conn.commit()
+    
+    return cur.fetchone()[0]
+def select_package(id):
+    conn = psycopg2.connect(url)
+    cur = conn.cursor()
+    cur.execute("SELECT * from packages where id='{}'".format(str(id)))
+    return cur.fetchone()
+def __selectall():
+    conn = psycopg2.connect(url)
+    cur = conn.cursor()
+    cur.execute("SELECT * from packages where id IS NOT NULL")
+    return cur.fetchall()
+def update_packages(id, location):
+    conn = psycopg2.connect(url)
+    cur = conn.cursor()
+    package = select_package(id)[2]
+    package.append({"date": datetime.now().strftime("%A %B %d %Y %r"), "location": location})
+    x = "UPDATE packages set locations = '{packages}' where id='{id}'".format(packages=str(package).replace("'", '"'), id=id)
+    print(x)
+    cur.execute(x)
+    conn.commit()
